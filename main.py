84d8c7737845173302d37a1e3ed141210b1a02c7
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2026-02-18 13:16:15 +0100

    Add command line arguments for ask and sync modes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import requests
+from datetime import datetime
 
 try:
     from secrets import PIXELA_USERNAME, PIXELA_TOKEN, TELEGRAM_ID, TELEGRAM_TOKEN
@@ -9,28 +11,77 @@ except ImportError:
     TELEGRAM_ID = os.environ.get("TELEGRAM_ID")
     TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+GRAPH_ID="graph1"
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN
+}
+TODAY = datetime.now()
 
 
 #TODO 1: De "Push" Functie (De Reminder)
-def send_telegram_message(text):
+def send_telegram_message():
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     params = {
         "chat_id": TELEGRAM_ID,
-        "text": text
+        "text": "Hoeveel minuten heb je vandaag gestudeerd?"
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
     print("Success! Check your phone!.")
 
-text = "Hoeveel minuten heb je vandaag gestudeerd?"
 
-send_telegram_message(text)
-# Maak een functie (bijv. send_reminder()) die niet wacht op een bericht, maar zelf het initiatief neemt.
-#
-#     Doel: De bot stuurt jou een bericht: "Hoeveel minuten heb je vandaag gestudeerd?"
-#
-#     Kennis: Gebruik de Telegram sendMessage method.
-#
-#     Variabelen: Je hebt je BOT_TOKEN en CHAT_ID nodig (deze kun je het beste als Environment Variables in GitHub Secrets zetten).
 
-#TODO 2: De "Pull" Functie (Input ophalen)
+def get_latest_input():
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = (response.json())
+
+    if not data["result"]:
+        return None, None
+
+    last_update = data["result"][-1]
+    last_update_id = last_update["update_id"]
+    text_return = last_update["message"]["text"].replace(",", ".")
+    try:
+        value = round(float(text_return))
+        return value, last_update_id
+
+    except ValueError:
+        return None, last_update_id
+
+def mark_as_read(last_id):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+    params = {"offset": last_id + 1}
+    requests.get(url, params=params)
+
+def update_graph():
+    pixel_update, last_id = get_latest_input()
+    if pixel_update is not None:
+        graph_update_endpoint = f"{PIXELA_ENDPOINT}/{PIXELA_USERNAME}/graphs/{GRAPH_ID}"
+        pixel_data = {
+        "date":	TODAY.strftime("%Y%m%d"),
+        "quantity":	pixel_update,
+        }
+        response = requests.post(url=graph_update_endpoint,json=pixel_data, headers=headers)
+        if response.ok:
+            print(f"Pixela updated! Status: {response.text}")
+            mark_as_read(last_id)  # Pas nu ruimen we de Telegram wachtrij op
+            send_telegram_message(f"✅ opgeslagen: {pixel_update} minuten in Pixela!")
+        else:
+            send_telegram_message(f"❌ Pixela error: {response.text}")
+    else:
+        send_telegram_message("⚠️ Geen geldige input in laatste update")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 :
+        if sys.argv[1] == "--ask":
+            send_telegram_message()
+        elif sys.argv[1] == "--sync":
+            update_graph()
+        else:
+            print("invalid command, valid options are: --ask or --sync")
+
+
+
